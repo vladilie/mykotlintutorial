e00250a40c8c548f167bb517850aeb535891209a
vlad ilie <vlad.ilie@artemispublishing.co.uk>
2019-10-13 11:11:36 +0300

    Refactor Person for shorter methods (elvis operator and 4 lines of code per method)

--- src/main/kotlin/mykotlintutorial/Person.kt
+++ src/main/kotlin/mykotlintutorial/Person.kt
@@ -5,24 +5,25 @@ import java.util.*
 data class Person(val id: Long,
                   val title: String,
                   val firstName: String,
-                  val surname:String,
+                  val surname: String,
                   val dateOfBirth: Calendar? = null) {
-    var age: Int = -1
-    get(){
-       return getAge(dateOfBirth)
-    }
 
-    companion object Static {
-        fun getAge(dateOfBirth: Calendar?):Int {
-            if (null == dateOfBirth) return -1
+    val age: Int
+        get() = getAge(dateOfBirth)
 
+    companion object {
+        fun getAge(dateOfBirth: Calendar?): Int =
+                getAge(getDayOfBirth(dateOfBirth), getYearOfBirth(dateOfBirth))
+
+        fun getAge(dayOfBirth: Int, yearOfBirth: Int): Int {
+            if (yearOfBirth == 0) return -1
             val today = GregorianCalendar()
-            var years = today.get(Calendar.YEAR) - dateOfBirth.get(Calendar.YEAR);
-            if (dateOfBirth.get(Calendar.DAY_OF_YEAR) >= today.get(Calendar.DAY_OF_YEAR)) {
-                years -= 1;
-            }
-
-            return years;
+            var years = today.get(Calendar.YEAR) - yearOfBirth
+            return if (dayOfBirth >= today.get(Calendar.DAY_OF_YEAR)) years - 1 else years
         }
+
+        fun getDayOfBirth(dateOfBirth: Calendar?): Int = dateOfBirth?.get(Calendar.DAY_OF_YEAR) ?: 0
+        fun getYearOfBirth(dateOfBirth: Calendar?): Int = dateOfBirth?.get(Calendar.YEAR) ?: 0
+
     }
 }
