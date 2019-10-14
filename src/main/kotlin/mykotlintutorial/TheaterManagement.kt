5912776671db7a60ef2c474e8ca973c0b4a4ba3d
vlad ilie <vlad.ilie@artemispublishing.co.uk>
2019-10-14 08:38:54 +0300

    shorter methods + added a test condition because the test was not complete

--- src/main/kotlin/mykotlintutorial/TheaterManagement.kt
+++ src/main/kotlin/mykotlintutorial/TheaterManagement.kt
@@ -6,17 +6,13 @@ data class Seat(val row: Int,
                 val column: Int,
                 val price: BigDecimal,
                 val description: String) {
-    override fun toString(): String {
-        return "Row $row-$column : $price"
-    }
+    override fun toString(): String = "Row $row-$column : $price"
 }
 
 class TheaterManagement {
     private val seats: List<Seat> = prepareSeats()
 
-    fun listCheapSeats(): List<Seat> {
-        return seats.filter { it.price == CHEAPEST_PRICE }
-    }
+    fun listCheapSeats(): List<Seat> = seats.filter { it.price == CHEAPEST_PRICE }
 
     companion object {
         @JvmField
@@ -27,32 +23,26 @@ class TheaterManagement {
         private const val RESTRICTED_VIEW = "restricted view"
         private const val BEST_VIEW = "best view"
 
-        private fun getPrice( row: Int, column: Int): BigDecimal {
-            return when {
-                row == 14 || row == 15 -> CHEAPEST_PRICE
-                column <=3 || column >= 34 -> BigDecimal(16.5)
-                row == 1 -> BigDecimal(21)
-                else -> BigDecimal(18)
-            }
+        private fun getPrice(row: Int, column: Int): BigDecimal = when {
+            row == 14 || row == 15 -> CHEAPEST_PRICE
+            column <= 3 || column >= 34 -> BigDecimal(16.5)
+            row == 1 -> BigDecimal(21)
+            else -> BigDecimal(18)
         }
 
-        private fun getDescription( row: Int, column: Int): String {
-            return when {
-                row == 14 -> CHEAP_ROW
-                row == 15 -> BACK_ROW
-                column <=3 || column >= 34 -> RESTRICTED_VIEW
-                row <= 2 -> BEST_VIEW
-                else -> STANDARD_VIEW
-            }
+        private fun getDescription(row: Int, column: Int): String = when {
+            row == 14 -> CHEAP_ROW
+            row == 15 -> BACK_ROW
+            column <= 3 || column >= 34 -> RESTRICTED_VIEW
+            row <= 2 -> BEST_VIEW
+            else -> STANDARD_VIEW
         }
 
-        private fun prepareSeats(): List<Seat> {
-            val theSeats = mutableListOf<Seat>()
-            for (row in 1..15)
-                for (column in 1..36)
-                    theSeats.add(Seat(row, column, getPrice(row, column), getDescription(row, column)))
-            return theSeats.toList()
-        }
+        private fun prepareSeats(): List<Seat> = (1..15).map { row ->
+            (1..36).map { column ->
+                Seat(row, column, getPrice(row, column), getDescription(row, column))
+            }
+        }.flatMap { it.asIterable() }
     }
 }
 
--- src/test/kotlin/mykotlintutorial/TheaterManagementTest.kt
+++ src/test/kotlin/mykotlintutorial/TheaterManagementTest.kt
@@ -10,13 +10,16 @@ class TheaterManagementTest {
     fun testCheapSeats() {
         val classUnderTest = TheaterManagement()
         assertNotNull(classUnderTest, "theater management should be built")
-        classUnderTest.listCheapSeats().forEach {
+        val listCheapSeats = classUnderTest.listCheapSeats()
+        assertEquals(2 * 36, listCheapSeats.size)
+        listCheapSeats.forEach {
             assertTrue(it.row == 14 || it.row == 15)
             assertTrue(it.column in 1..36)
             assertEquals(it.price, TheaterManagement.CHEAPEST_PRICE)
             assertTrue(it.description == TheaterManagement.CHEAP_ROW ||
                     it.description == TheaterManagement.BACK_ROW)
         }
+
     }
 
 }
